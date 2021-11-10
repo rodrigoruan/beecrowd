2c9dff9027f1a419b08f71eb51a635426c534e23
Rodrigo <rodrigopython16@gmail.com>
2021-11-10 18:25:50 -0300

    Added 1272

---
+++ 1272/script.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for(let i = 1; i <= n; i += 1) {
+  const s = lines[i].split(' ')
+  const result = s.map(v => v[0] || '').filter(v => v).join('');
+  console.log(result)
+}
