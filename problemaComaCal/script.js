d337e92d311d24e0e598662a9f2afefc235ad356
Rodrigo <rodrigopython16@gmail.com>
2021-11-10 00:15:21 -0300

    Added 2694

---
+++ problemaComaCal/script.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for (let i = 1; i <= n; i++) {
+  const nums = lines[i].match(/\d+/g).reduce((acc, curr) => acc + +curr, 0);
+  console.log(nums);
+}
