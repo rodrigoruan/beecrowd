bdcdd532419c339e93439c6756c1b2ede454156e
Rodrigo <rodrigopython16@gmail.com>
2021-11-11 23:12:35 -0300

    added 2242

---
+++ 2242/script.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const str = lines[0].replace(/[^aeiou]/g, '');
+const result = str === str.split('').reverse().join('')
+
+console.log(result ? 'S' : 'N');
