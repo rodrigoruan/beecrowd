deb7067cde29d0afb318b873f0e36edb9ea5941e
Rodrigo <rodrigopython16@gmail.com>
2021-11-12 00:40:55 -0300

    added 1192

---
+++ 1192/script.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for (let i = 1; i <= n; i += 1) {
+  let [a, l, b] = lines[i].split('');
+  (a = Number(a)), (b = Number(b));
+
+  if (a === b) {
+    console.log(b * a);
+  } else if (l.toUpperCase() === l) {
+    console.log(b - a);
+  } else {
+    console.log(a + b);
+  }
+}
