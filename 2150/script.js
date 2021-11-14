96a6fb76ad565ffa00d8f793217e24ebd91fcb6c
Rodrigo <rodrigopython16@gmail.com>
2021-11-14 19:29:42 -0300

    added 2150

---
+++ 2150/script.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+for(let i = 0; lines[i]; i += 2) {
+  const letters = lines[i]
+  const word = lines[i+1]
+
+  const reg = new RegExp(`[^${letters}]`, 'g')
+  console.log(word.replace(reg, '').length)
+}
