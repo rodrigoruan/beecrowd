334933234a8a59ea224b0ad66bc4e9a9d4fabcab
Rodrigo <rodrigopython16@gmail.com>
2021-11-16 12:33:25 -0300

    added 2381

---
+++ 2381/script.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const [n, res] = lines[0].split(' ').map(Number);
+
+const arr = [];
+
+for (let i = 1; i <= n; i++) {
+  arr.push(lines[i]);
+}
+
+console.log(arr.sort((a, b) => a.localeCompare(b))[res - 1]);
