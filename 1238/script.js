28bfef8f95d79ddc826fcbaaab64461a2b6c6cc8
Rodrigo <rodrigopython16@gmail.com>
2021-11-17 12:37:38 -0300

    added 1238

---
+++ 1238/script.js
@@ -0,0 +1,18 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for (let i = 1; i <= n; i += 1) {
+  const [a, b] = lines[i].split(' ');
+  const maxLength = Math.max(a.length, b.length);
+
+  const arr = [];
+
+  for (let j = 0; j < maxLength; j += 1) {
+    if (a[j]) arr.push(a[j]);
+    if (b[j]) arr.push(b[j]);
+  }
+
+  console.log(arr.join(''));
+}
