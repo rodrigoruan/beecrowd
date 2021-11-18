b5bce98cb42ece47295262de64d4e823e82dc513
Rodrigo <rodrigopython16@gmail.com>
2021-11-17 22:03:09 -0300

    added 1551

---
+++ 1551/script.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for (let i = 1; i <= n; i += 1) {
+  const actual = lines[i].replace(/[^a-z]/gi, '');
+  const letters = [...new Set(actual)].length;
+
+  if (letters === 26) {
+    console.log('frase completa');
+  } else if (letters >= 13) {
+    console.log('frase quase completa');
+  } else {
+    console.log('frase mal elaborada');
+  }
+}
