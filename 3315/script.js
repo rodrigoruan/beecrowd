9edc2de8e9c72244f4d5778cd31a32e2aabd6887
Rodrigo <rodrigopython16@gmail.com>
2021-11-24 16:11:07 -0300

    added 3315

---
+++ 3315/script.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Math.max(
+  ...lines.map((value) =>
+    value.split(' ').reduce((acc, curr) => acc + Number(curr), 0)
+  )
+);
+
+console.log(`${n} = ${n.toString(2)}`);
