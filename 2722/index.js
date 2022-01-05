2a983920fd4fe0b6621324f63dad191e2b7238b9
Rodrigo <rodrigopython16@gmail.com>
2022-01-04 23:32:37 -0300

    2722

---
+++ 2722/index.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+const n = Number(lines[0]);
+
+for(let i = 1; i <= n * 2; i += 2) {
+  const a = lines[i];
+  const b = lines[i+1];
+
+  let str = "";
+
+  for(let j = 0; j < a.length; j += 2) {
+    str += a.substring(j, j + 2) + b.substring(j, j + 2);
+  }
+
+  console.log(str);
+}
